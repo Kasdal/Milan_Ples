7d7337c35b08b5b85033ff8dbaf4b235567d7f8c
Milan <plesmilan@hotmail.com>
2020-04-08 01:35:18 +0100

    Changes to code

--- src/AdminWorker.java
+++ src/AdminWorker.java
@@ -1,5 +1,25 @@
 public class AdminWorker extends Employee{
 
+    private int bonus;
 
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        if(Utilities.validIntNonNegative(bonus))
+            this.bonus = bonus;
+        else
+            this.bonus = 0;
+    }
+
+
+
+    public AdminWorker(double hoursWorked, double hourlyRate,int bonus){
+        super(hoursWorked,hourlyRate);
+    }
+    public double calculateSallary(){
+        return 0;
+    }
 
 }
--- src/SalesWorker.java
+++ src/SalesWorker.java
@@ -1,3 +1,22 @@
 public class SalesWorker extends Employee{
 
+    private int bonus;
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+            this.bonus = bonus;
+//probably write new validation method in util?
+    }
+
+
+
+    public SalesWorker(double hoursWorked, double hourlyRate,int bonus){
+        super(hoursWorked,hourlyRate);
+    }
+    public double calculateSallary(){
+        return 0;
+    }
 }
